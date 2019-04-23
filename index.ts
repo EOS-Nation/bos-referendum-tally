3d51a8367e0ea80758dc513c10a426b13c84ae6d
Denis <carriere.denis@gmail.com>
2019-04-22 22:24:54 -1000

    Add tallies

--- index.ts
+++ index.ts
@@ -1,29 +1,40 @@
 import * as path from "path";
-import * as fs from "fs";
 import * as write from "write-json-file";
-import * as load from "load-json-file";
 import { CronJob } from "cron";
-import { Vote, Proposal, Voters } from "./src/interfaces";
+import { Vote, Proposal, Voters, Delband } from "./src/interfaces";
 import { rpc, CHAIN, CONTRACT_FORUM } from "./src/config";
+import { filterVotersByVotes } from "./src/tallies";
 import { get_table_voters, get_table_vote, get_table_proposal } from "./src/get_tables";
+import { disjoint } from "./src/utils";
 
 // Base filepaths
 const basepath = path.join(__dirname, "data", CHAIN);
 
 // Global containers
-let vote: Vote[] = [];
+let votes: Vote[] = [];
 let voters: Voters[] = [];
-let proposal: Proposal[] = [];
+let proposals: Proposal[] = [];
+let votes_owner: Set<string> = new Set();
+let voters_owner: Set<string> = new Set();
+let delband: Delband[] = [];
 
 /**
  * Sync `eosio` tables
  */
 async function syncEosio() {
-    console.log("vote", vote.length)
     const {head_block_num} = await rpc.get_info()
     console.log("head_block_num:", head_block_num)
 
-    voters = await get_table_voters();
+    // fetch `eosio` voters
+    voters = filterVotersByVotes(await get_table_voters(), votes);
+    voters_owner = new Set(voters.map((row) => row.owner));
+
+    // Retrieve `staked` from accounts that have not yet voted for BPs
+    const owners_without_stake = disjoint(votes_owner, voters_owner)
+    console.log(owners_without_stake, owners_without_stake.size);
+    // delband = await get_table_delband(votes_owner, voters_owner);
+
+    // Save JSON
     save(path.join(basepath, "eosio", "voters"), head_block_num, voters);
 }
 
@@ -34,11 +45,16 @@ async function syncForum() {
     const {head_block_num} = await rpc.get_info()
     console.log("head_block_num:", head_block_num)
 
-    vote = await get_table_vote();
-    save(path.join(basepath, CONTRACT_FORUM, "vote"), head_block_num, vote);
+    // fetch `eosio.forum` votes
+    votes = await get_table_vote();
+    votes_owner = new Set(votes.map((row) => row.voter));
 
-    proposal = await get_table_proposal();
-    save(path.join(basepath, CONTRACT_FORUM, "proposal"), head_block_num, proposal);
+    // fetch `eosio.forum` proposal
+    proposals = await get_table_proposal();
+
+    // Save JSON
+    save(path.join(basepath, CONTRACT_FORUM, "vote"), head_block_num, votes);
+    save(path.join(basepath, CONTRACT_FORUM, "proposal"), head_block_num, proposals);
 }
 
 /**
--- src/utils.ts
+++ src/utils.ts
@@ -30,3 +30,30 @@ export function createHash(json: any) {
     const data = JSON.stringify(json);
     return crypto.createHash('md5').update(data).digest("hex");
 }
+
+/**
+ * Parse Token String
+ *
+ * @param {string} tokenString Token String (eg: "10.0 EOS")
+ * @returns {object} Amount & Symbol
+ * @example
+ * parseTokenString("10.0 EOS") //=> {amount: 10.0, symbol: "EOS"}
+ */
+export function parseTokenString(tokenString: string) {
+    const [amountString, symbol] = tokenString.split(" ");
+    const amount = parseFloat(amountString);
+    return {amount, symbol};
+}
+
+/**
+ * Disjoint
+ *
+ * Two sets are said to be disjoint sets if they have no element in common.
+ */
+export function disjoint(source: Set<string>, target: Set<string>) {
+    const result = new Set<string>();
+    source.forEach(item => {
+        if (!target.has(item)) result.add(item);
+    })
+    return result;
+}
